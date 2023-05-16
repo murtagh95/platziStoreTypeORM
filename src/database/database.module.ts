a596f108ca2ea8cb5c7cd89b02ce3217ad3fc051
Murtagh95 <nec.catalano@gmail.com>
2023-05-16 08:42:59 -0300

    feat(database.module): add typeorm config.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,36 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+
+import config from '../config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, port, host, dbName, password } = configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: dbName,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', TypeOrmModule],
 })
 export class DatabaseModule {}
