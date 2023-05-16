92ad015d69b28483852f2aefc803c1c70a56a776
Murtagh95 <nec.catalano@gmail.com>
2023-05-16 08:41:54 -0300

    feat(config): variables are added for the porstreges bd.

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,13 @@ export default registerAs('config', () => {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
     },
+    postgres: {
+      dbName: process.env.DATABASE_NAME,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      host: process.env.DATABASE_HOST,
+    },
     apiKey: process.env.API_KEY,
   };
 });
